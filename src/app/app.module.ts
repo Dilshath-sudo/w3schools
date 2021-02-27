bf4bc1d6c8073123e10248c070204f647ac09245
Mohamed Dilshath <dilshathwpg@gmail.com>
2021-02-27 09:24:22 +0530

    TopBar & SideBar Components Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule
